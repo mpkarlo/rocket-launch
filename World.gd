1a8f47393d8de2ec4d03a82b45b156c4f18ae922
Karlo Melendez-Pena <mgkarlo@gmail.com>
2020-07-06 18:30:22 -0400

    TextureButton Node + Signals

--- World.gd
+++ World.gd
@@ -3,4 +3,8 @@ extends Node
 onready var animationPlayer = $AnimationPlayer
 
 func _ready():
+	pass # Replace with function body.
+
+
+func _on_LaunchButton_pressed():
 	animationPlayer.play("Launch")
